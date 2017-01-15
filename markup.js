dc65a59e807748c54c5552344e20b0139531e50d
Lucas Wagner <lowagner@gmail.com>
2017-01-14 22:33:42 -0600

    initial stuff

---
+++ markup.js
@@ -0,0 +1,8 @@
+(function () { // create anonymous function
+// BEGIN normal javascript
+var EditMarkup = document.getElementById("EditMarkup");
+if (!EditMarkup) return;
+var ShowMarkup = document.getElementById("ShowMarkup");
+if (!ShowMarkup) return;
+// END
+})() // function enclosure
